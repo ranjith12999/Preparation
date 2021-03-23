d10cb870f83adadd09df536f426b270813315313
Ranjith_kumar <64999477+ranjith12999@users.noreply.github.com>
2021-03-23 22:50:03 +0530

    Update code2(#26).cpp

--- code2(#26).cpp
+++ code2(#26).cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -14,3 +19,15 @@ public:
         return j+1;
     }
 };
+
+int main()
+{
+    Solution s;
+    vector<int> mat{1,1,2};
+    int res = s.removeDuplicates(mat);
+   for(int i=0 ; i<res;i++){
+        cout<<" "<<mat[i];
+    }
+
+    return 0;
+}
